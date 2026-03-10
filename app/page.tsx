901e000c7e72fe72f83bbc310186737da976e2ae
devaziz2 <aziz.naseer@tixertech.com>
2026-03-11 00:04:51 +0500

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div>learning git</div>
+      <div className="flex flex-col gap-2">
+        <div>learning git</div>
+        <div>on dev branch</div>
+      </div>
     </div>
   );
 }
